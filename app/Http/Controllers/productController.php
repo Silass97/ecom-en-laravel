dff2397a24871bb2637c183988ca35cb6c31b638
silass97 <alassanesilue40@gmail.com>
2021-06-04 11:55:29 +0200

    cartlist and modification on headerand master

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Cart;
 use Session;
+use Illuminate\Support\Facades\DB;
 class ProductController extends Controller
 {
     //
@@ -41,11 +42,21 @@ class ProductController extends Controller
         return redirect('/login');
        }
     }
-  static function cartItem()
+    static function cartItem()
    {
        $userId=Session::get('user')['id'];
        return Cart::where('user_id',$userId)->count();
    }
+    function cartList()
+    {
+        $userId=Session::get('user')['id'];
+        $products= DB::table('cart')
+        ->join ('products','cart.product_id','=','products.id')
+        ->where('cart.user_id',$userId)
+        ->select('products.*')
+        ->get();
 
+        return view('cartlist',['products'=>$products]);
+    }
 
 }
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -49,7 +49,11 @@ height:400px !important
 .search-box{
     width: 500px !important
 }
-
+.cart-list-devider{
+    border-bottom: 1px solid #ccc;
+    margin-bottom:20px;
+    padding-bottom:20px;
+}
 
 
 </style>
